0b95cfff28b03e02c6ff5f10cc84f33bba5556f3
ayokunle ogunfidodo <89479717+fidodo@users.noreply.github.com>
2025-02-26 14:31:35 +0200

    delete function coimleted

--- app/components/IdeasSection.js
+++ app/components/IdeasSection.js
@@ -1,3 +1,197 @@
+// "use client";
+// import { useState } from "react";
+// import { DndContext, closestCenter } from "@dnd-kit/core";
+// import {
+//   SortableContext,
+//   arrayMove,
+//   verticalListSortingStrategy,
+//   useSortable,
+// } from "@dnd-kit/sortable";
+// import { CSS } from "@dnd-kit/utilities";
+// import { Trash2 } from "lucide-react";
+
+// const SortableItem = ({ id, value, onDoubleClick, timestamp, section }) => {
+//   const { attributes, listeners, setNodeRef, transform, transition } =
+//     useSortable({ id });
+
+//   const style = {
+//     transform: CSS.Transform.toString(transform),
+//     transition,
+//   };
+
+//   const backgroundColor = section === "ideas" ? "bg-slate-200" : "bg-lime-100";
+
+//   return (
+//     <div
+//       ref={setNodeRef}
+//       style={style}
+//       {...attributes}
+//       {...listeners}
+//       onDoubleClick={onDoubleClick}
+//       className={`${backgroundColor} p-4 shadow rounded-lg mb-2 cursor-pointer flex justify-between items-center`}
+//     >
+//       <div>
+//         <p className="text-sm text-gray-900 dark:text-gray-400">{timestamp}</p>
+//         <span>{value}</span>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const IdeasSection = () => {
+//   const [ideas, setIdeas] = useState([]);
+//   const [done, setDone] = useState([]);
+//   const [newIdea, setNewIdea] = useState("");
+
+//   const addIdea = () => {
+//     if (newIdea.trim()) {
+//       const capitalizedIdea = capitalizeFirstLetter(newIdea);
+//       const timestamp = new Date().toLocaleString();
+//       setIdeas([...ideas, { text: capitalizedIdea, timestamp }]);
+//       setNewIdea("");
+//     }
+//   };
+
+//   const moveToDone = (idea) => {
+//     setIdeas(ideas.filter((item) => item !== idea));
+//     setDone([...done, idea]);
+//   };
+
+//   const moveToIdeas = (idea) => {
+//     setDone(done.filter((item) => item !== idea));
+//     setIdeas([...ideas, idea]);
+//   };
+
+//   const deleteIdea = (ideaToDelete) => {
+//     setIdeas(ideas.filter((idea) => idea.text !== ideaToDelete.text));
+//   };
+
+//   const deleteDone = (ideaToDelete) => {
+//     setDone(done.filter((item) => item.text !== ideaToDelete.text));
+//   };
+
+//   function capitalizeFirstLetter(sentence) {
+//     if (!sentence) return "";
+//     return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+//   }
+
+//   const onDragEnd = (event) => {
+//     const { active, over } = event;
+
+//     if (!over) return;
+
+//     const activeContainer = active.data.current?.sortable.containerId;
+//     const overContainer = over.data.current?.sortable.containerId;
+
+//     if (activeContainer === overContainer) {
+//       if (active.id !== over.id) {
+//         if (activeContainer === "ideas") {
+//           const oldIndex = ideas.findIndex((idea) => idea === active.id);
+//           const newIndex = ideas.findIndex((idea) => idea === over.id);
+//           setIdeas(arrayMove(ideas, oldIndex, newIndex));
+//         } else if (activeContainer === "done") {
+//           const oldIndex = done.findIndex((idea) => idea === active.id);
+//           const newIndex = done.findIndex((idea) => idea === over.id);
+//           setDone(arrayMove(done, oldIndex, newIndex));
+//         }
+//       }
+//     } else {
+//       if (activeContainer === "ideas" && overContainer === "done") {
+//         setIdeas(ideas.filter((idea) => idea !== active.id));
+//         setDone([...done, active.id]);
+//       } else if (activeContainer === "done" && overContainer === "ideas") {
+//         setDone(done.filter((idea) => idea !== active.id));
+//         setIdeas([...ideas, active.id]);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="space-y-4">
+//       <div>
+//         <input
+//           type="text"
+//           value={newIdea}
+//           onChange={(e) => setNewIdea(e.target.value)}
+//           className="border p-2 rounded-lg w-full mb-2 text-gray-900"
+//           placeholder="Add a new idea"
+//         />
+//         <button
+//           onClick={addIdea}
+//           className="bg-green-500 text-gray-900 px-4 py-2 rounded-lg mb-4 hover:bg-green-600 transition-colors"
+//         >
+//           Add Idea
+//         </button>
+//       </div>
+//       <div className="text-gray-900">
+//         <h2 className="inline-block bg-red-400 text-gray-800 px-4 py-2 rounded-md text-sm font-semibold mb-2">
+//           Ideas
+//         </h2>
+//         <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+//           <SortableContext
+//             items={ideas.map((idea) => idea.text)} 
+//             strategy={verticalListSortingStrategy}
+//             id="ideas"
+//           >
+//             {ideas.map((idea, index) => (
+//               <div key={index} className="flex items-center gap-2">
+//                 <SortableItem
+//                   id={idea.text}
+//                   value={idea.text}
+//                   timestamp={idea.timestamp}
+//                   onDoubleClick={() => moveToDone(idea)}
+//                   section="ideas"
+//                 />
+//                 <button
+//                   onClick={() => deleteIdea(idea)}
+//                   className="text-red-500 hover:text-red-700"
+//                 >
+//                   <Trash2 className="h-5 w-5" />
+//                 </button>
+//               </div>
+//             ))}
+//           </SortableContext>
+//         </DndContext>
+//       </div>
+//       <div className="text-gray-900">
+//         <div>
+//           <h2 className="inline-block bg-lime-400 text-gray-800 px-4 py-2 rounded-md text-sm font-semibold mb-2">
+//             Done
+//           </h2>
+//         </div>
+//         <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+//           <SortableContext
+//             items={done.map((item) => item.text)}
+//             strategy={verticalListSortingStrategy}
+//             id="done"
+//           >
+//             {done.map((idea, index) => (
+//               <div key={index} className="flex items-center gap-2">
+//                 <SortableItem
+//                   id={idea.text}
+//                   value={idea.text}
+//                   timestamp={idea.timestamp}
+//                   onDoubleClick={() => moveToIdeas(idea)}
+//                   section="done"
+//                 />
+//                 <button
+//                   onClick={() => deleteDone(idea)}
+//                   className="text-red-500 hover:text-red-700"
+//                 >
+//                   <Trash2 className="h-5 w-5" />
+//                 </button>
+//               </div>
+//             ))}
+//           </SortableContext>
+//         </DndContext>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default IdeasSection;
+
+
 "use client";
 import { useState } from "react";
 import { DndContext, closestCenter } from "@dnd-kit/core";
@@ -8,8 +202,9 @@ import {
   useSortable,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { Trash2 } from "lucide-react";
 
-const SortableItem = ({ id, value, onDoubleClick }) => {
+const SortableItem = ({ id, value, onDoubleClick, timestamp, section }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
 
@@ -18,6 +213,8 @@ const SortableItem = ({ id, value, onDoubleClick }) => {
     transition,
   };
 
+  const backgroundColor = section === "ideas" ? "bg-slate-200" : "bg-lime-100";
+
   return (
     <div
       ref={setNodeRef}
@@ -25,9 +222,12 @@ const SortableItem = ({ id, value, onDoubleClick }) => {
       {...attributes}
       {...listeners}
       onDoubleClick={onDoubleClick}
-      className="bg-white p-4 shadow rounded-lg mb-2 cursor-pointer"
+      className={`${backgroundColor} p-4 shadow rounded-lg mb-2 cursor-pointer flex justify-between items-center w-full`}
     >
-      {value}
+      <div>
+        <p className="text-sm text-gray-900 dark:text-gray-400">{timestamp}</p>
+        <span>{value}</span>
+      </div>
     </div>
   );
 };
@@ -39,7 +239,9 @@ const IdeasSection = () => {
 
   const addIdea = () => {
     if (newIdea.trim()) {
-      setIdeas([...ideas, newIdea]);
+      const capitalizedIdea = capitalizeFirstLetter(newIdea);
+      const timestamp = new Date().toLocaleString();
+      setIdeas([...ideas, { text: capitalizedIdea, timestamp }]);
       setNewIdea("");
     }
   };
@@ -53,6 +255,19 @@ const IdeasSection = () => {
     setDone(done.filter((item) => item !== idea));
     setIdeas([...ideas, idea]);
   };
+
+  const deleteIdea = (ideaToDelete) => {
+    setIdeas(ideas.filter((idea) => idea.text !== ideaToDelete.text));
+  };
+
+  const deleteDone = (ideaToDelete) => {
+    setDone(done.filter((item) => item.text !== ideaToDelete.text));
+  };
+
+  function capitalizeFirstLetter(sentence) {
+    if (!sentence) return "";
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  }
 
   const onDragEnd = (event) => {
     const { active, over } = event;
@@ -86,7 +301,7 @@ const IdeasSection = () => {
   };
 
   return (
-    <div className="space-y-4">
+    <div className="w-[80%] mx-auto space-y-4">
       <div>
         <input
           type="text"
@@ -103,48 +318,63 @@ const IdeasSection = () => {
         </button>
       </div>
       <div className="text-gray-900">
-        {" "}
-        <h2 className="inline-block bg-red-400 text-gray-800 px-4 py-2 rounded-md  text-sm font-semibold mb-2">
+        <h2 className="inline-block bg-red-400 text-gray-800 px-4 py-2 rounded-md text-sm font-semibold mb-2">
           Ideas
         </h2>
         <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
           <SortableContext
-            items={ideas}
+            items={ideas.map((idea) => idea.text)}
             strategy={verticalListSortingStrategy}
             id="ideas"
           >
             {ideas.map((idea, index) => (
-              <SortableItem
-                key={index}
-                id={idea}
-                value={idea}
-                onDoubleClick={() => moveToDone(idea)}
-              />
+              <div key={index} className="flex items-center gap-2 w-full">
+                <SortableItem
+                  id={idea.text}
+                  value={idea.text}
+                  timestamp={idea.timestamp}
+                  onDoubleClick={() => moveToDone(idea)}
+                  section="ideas"
+                />
+                <button
+                  onClick={() => deleteIdea(idea)}
+                  className="text-red-500 hover:text-red-700"
+                >
+                  <Trash2 className="h-5 w-5" />
+                </button>
+              </div>
             ))}
           </SortableContext>
         </DndContext>
       </div>
       <div className="text-gray-900">
-        <div >
-          {" "}
+        <div>
           <h2 className="inline-block bg-lime-400 text-gray-800 px-4 py-2 rounded-md text-sm font-semibold mb-2">
             Done
           </h2>
         </div>
-        {/* Drag & Drop Context for Done */}
         <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
           <SortableContext
-            items={done}
+            items={done.map((item) => item.text)}
             strategy={verticalListSortingStrategy}
             id="done"
           >
             {done.map((idea, index) => (
-              <SortableItem
-                key={index}
-                id={idea}
-                value={idea}
-                onDoubleClick={() => moveToIdeas(idea)}
-              />
+              <div key={index} className="flex items-center gap-2 w-full">
+                <SortableItem
+                  id={idea.text}
+                  value={idea.text}
+                  timestamp={idea.timestamp}
+                  onDoubleClick={() => moveToIdeas(idea)}
+                  section="done"
+                />
+                <button
+                  onClick={() => deleteDone(idea)}
+                  className="text-red-500 hover:text-red-700"
+                >
+                  <Trash2 className="h-5 w-5" />
+                </button>
+              </div>
             ))}
           </SortableContext>
         </DndContext>
